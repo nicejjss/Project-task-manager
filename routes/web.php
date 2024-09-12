a0419f3eb9cfb7cde5d4615eccb26d052a67c9df
nicejjss <locdaoduc2002@gmail.com>
2024-09-13 00:43:15 +0700

    feat(#1_authentication): adding authentication

--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,38 @@
 <?php
 
+use App\Http\Controllers\Authentication\ActiveController;
+use App\Http\Controllers\Authentication\LoginController;
+use App\Http\Controllers\Authentication\ResetPasswordController;
+use App\Http\Controllers\Authentication\SignUpController;
 use Carbon\Carbon;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use Laravel\Socialite\Facades\Socialite;
+
+
+Route::get('/login', function () {
+    return view('authentication.login');
+})->name('login');
+
+Route::get('/signup', function () {
+    return view('authentication.signup');
+})->name('signup');
+
+Route::get('/forget_password', function () {
+    return view('authentication.forget_password');
+})->name('forget_password');
+
+Route::post('/login', [LoginController::class, 'login']);
+Route::post('/signup', [SignUpController::class, 'signUp']);
+Route::post('/active_account', [ActiveController::class, 'active']);
+Route::post('/send_mail_reset', [ResetPasswordController::class, 'sendMail']);
+Route::post('reset_password', [ResetPasswordController::class, 'reset_password']);
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/oauth/google/callback', function () {
+Route::get('/oauth/google/callback', function (Request $request) {
     $user = Socialite::driver('google')->user();
     dd($user, $user->getId());
 });
@@ -17,7 +41,7 @@ Route::get('/oauth/google/login', function () {
     return Socialite::driver('google')->with(['access_type' => 'offline'])
         ->scopes(['https://www.googleapis.com/auth/calendar.events'])
         ->redirect();
-});
+})->name('google.login');
 
 Route::get('/calendar', function () {
     $client = new Google_Client();
@@ -58,7 +82,7 @@ Route::get('/calendar', function () {
 
 
 Route::get('/event-view', function () {
-   return view('newevent', ['key' => config('broadcasting.connections.pusher.key')]);
+    return view('newevent', ['key' => config('broadcasting.connections.pusher.key')]);
 });
 
 Route::get('/event-view-push', function () {
